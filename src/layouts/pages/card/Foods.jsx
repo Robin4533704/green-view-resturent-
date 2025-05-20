2fe29e1eeb9db82c3821d38f2f9b708062edf289
Robin4533704 <robinhossen8428@gmail.com>
2025-05-20 20:27:07 +0600

    secend commit

--- src/layouts/pages/card/Foods.jsx
+++ src/layouts/pages/card/Foods.jsx
@@ -12,7 +12,7 @@ const Foods = ({ data }) => {
 
   return (
    <div>
-    <h1 className='text-4xl font-bold text-center  '>Italiyan Foods</h1>
+    <h1 className='text-4xl font-bold text-center  mt-6 mb-6'>Italiyan Foods</h1>
      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 '>
      
       {data.map((foodcategory) => (
